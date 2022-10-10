c43271867ccfb8a8cc74320cee206b9edd2a7e13
matej <matej.basta@email.cz>
2022-10-10 17:14:52 +0200

    Order class implemented

---
+++ Order.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Order {
+    
+    protected List<Food> ordered;
+
+    public Order() {
+        ordered = new ArrayList<>();
+    }
+
+    public void addFood(Food food) {
+        ordered.add(food);
+    }
+
+    public int total() {
+        int total = 0;
+        for (Food food : ordered) {
+            total += food.getPrice();
+        }
+        return total;
+    }
+
+    public void display() {
+        for (Food food : ordered) {
+            food.display();
+            System.out.println("");
+        }
+        System.out.println(total() + " kr" + " TOTAL" );
+    }
+
+}
